05bca0ffc4bfe929159e61f32a66c4d0c1a6406e
SEB214 <SEB214@CILABS-119A-06.scilsnet.rutgers.edu>
2017-10-05 22:42:59 -0400

    Finished Class
    Thing was messed up at the end

---
+++ Collecium/Collecium/Assets/2D Assets/_Scripts/BulletController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BulletController : MonoBehaviour {
+
+	public float speed;
+	public GameObject Bullet; //calls the bullet object
+
+	private PlayerControlScript player; //calls the playercontroller
+	public GameObject slimeDeathEffect;
+
+	AudioSource audio;
+	public AudioClip slimeDeathSound; //set of death effect and death sound are in a pair when facing multiple enimies
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerControlScript> ();
+
+		audio = GetComponent<AudioSource> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, 0.0f); //0.0 is used to define the y. Speed is used to define the x. 
+		if (player.transform.localScale.x < 0) {
+			speed = -speed;
+		}
+		speed = 20f;
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag =="Enemy")
+		{
+			audio.PlayOneShot (slimeDeathSound, 1.0f);	
+			Instantiate (slimeDeathEffect, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+		}
+		if (other.tag == "Bullet Container") {
+			Destroy (gameObject);
+		}
+}
+}
---
+++ Collecium/Collecium/Assets/2D Assets/_Scripts/Stomp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic; //allows call to core routine
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Stomp : MonoBehaviour {
+
+	public AudioClip enemyDeathSound;
+	public float delay;
+
+	AudioSource audio;
+	private Rigidbody2D playerBody;
+	public float bounceOnEnemy;
+
+	public GameObject enemyDeathEffect;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		playerBody = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D> (); // Looks for the game object that has the specific tag, and wants to maniuplate the component inside that object as RigiBody
+
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+
+			StartCoroutine ("DelayDeath"); //lags cpu and makes things slow
+			Instantiate (enemyDeathEffect, coll.transform.position, coll.transform.rotation);
+			playerBody.velocity = new Vector2 (playerBody.velocity.x, bounceOnEnemy);
+			//problem without putting anything below this is that no sound gets played since everything happens at the same time.
+		}
+	}
+
+	IEnumerator DelayDeath () { 
+		audio.PlayOneShot (enemyDeathSound, 1.0f);
+		yield return new WaitForSeconds (delay);
+		Destroy (transform.parent.gameObject); //destroys the parent of the slime instead of the box
+	}
+}
